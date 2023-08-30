cf4f4f95205c96b6cf5b2398b3cd10748ac8b283
Andrei Gerasimenko <gerasimenko_andrei@bk.ru>
2023-08-30 23:10:42 +0700

    Add titles, test css

--- src/components/StartView/StartView.tsx
+++ src/components/StartView/StartView.tsx
@@ -5,7 +5,17 @@ import jpg from "images/photo.jpg";
 const StartView = () => {
 	return (
 		<StartViewContainer>
+			{/* <div className="background-image">
+				<h1 className="name">Герасименко Андрей</h1>
+				<p className="specialization">
+					Являюсь <span className="selected">Frontend разработчиком</span>
+				</p>
+			</div> */}
 			<img src={jpg} className="background-image" alt="background-photo" />
+			<h1 className="name">Герасименко Андрей</h1>
+			<p className="specialization">
+				Являюсь <span className="selected">Frontend разработчиком</span>
+			</p>
 		</StartViewContainer>
 	);
 };
